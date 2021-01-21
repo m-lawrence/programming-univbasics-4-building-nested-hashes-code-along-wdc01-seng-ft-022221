508cbbe622473dafcfbe1970369d97d781ccc527
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-21 15:23:04 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,10 +2,10 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague", :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        #your /value pairs here
         },
       :hero => {
         #your key/value pairs here
